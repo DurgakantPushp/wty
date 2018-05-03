bdbe7bb80602368e2faee6ed75f6549aa880ef84
Ajay Kumar <akumar@tekion.com>
2018-05-03 22:33:29 +0530

    fiter by sender and recipient integrated

--- client/src/components/Blockchain.vue
+++ client/src/components/Blockchain.vue
@@ -1,16 +1,49 @@
 <template>
   <div>
     <div class="container-responsive" style="margin: 20px">
-      <h2> Filter user</h2>
-      <label>user name (sender)</label>
-      <input
-        v-model="userName"
-        placeholder="user name"
+      <h2> Filter user by sender</h2>
+      <label>sender</label>
+      <select
+        id="sender"
+        v-for="sender of senders"
+        :key="sender.id"
       >
+        <option>{{sender}}</option>
+      </select>
       <button
         class="btn btn-primary"
-        @click="userGrats">
-        show Gratitudes
+        v-if="sndEnabled"
+        @click="filterBySender">
+        Gratitude as per sender
+      </button>
+      <button
+        class="btn btn-danger"
+        v-else
+        @click="filterResetBySender">
+        Reset
+      </button>
+    </div>
+    <div class="container-responsive" style="margin: 20px">
+      <h2> Filter user by recipient</h2>
+      <label>recipient</label>
+      <select
+        id="recipient"
+        v-for="recipient of recipients"
+        :key="recipient.id"
+      >
+        <option>{{recipient}}</option>
+      </select>
+      <button
+        class="btn btn-primary"
+        v-if="rcptEnabled"
+        @click="filterByRecipient">
+        Gratitude as per recipient
+      </button>
+      <button
+        class="btn btn-danger"
+        v-else
+        @click="filterResetByRecipient">
+        Reset
       </button>
     </div>
     <table class="w3-table w3-striped w3-bordered w3-border">
@@ -42,7 +75,10 @@ import AdminGratitude from './AdminGratitude'
 export default {
   data () {
     return {
-      userName: ''
+      sender: '',
+      recipient: '',
+      sndEnabled: true,
+      rcptEnabled: true
     }
   },
   components: {
@@ -51,22 +87,37 @@ export default {
   },
 
   computed: {
-    blocks() {
+    blocks () {
       return store.state.blocks
+    },
+    senders () {
+      return store.state.senders
+    },
+    recipients () {
+      return store.state.recipients
     }
   },
+
   methods: {
-    userGrats () {
-      console.log('user gratitudes for user', this.userName)
-      let tmpBlocks = []
-      for (let block of this.blocks) {
-        let grat = JSON.parse(atob(block.data))
-        if (grat.sender === this.sender) {
-          tmpBlocks.push(block)
-          console.log('block pushed', block)
-        }
-      }
-      this.blocks = tmpBlocks
+    filterBySender () {
+      this.sender = $('#sender').val()
+      console.log('user gratitudes for sender', this.sender)
+      store.commit('FilterBySender', this.sender)
+      this.sndEnabled = false
+    },
+    filterByRecipient () {
+      this.recipient = $('#recipient').val()
+      console.log('gratitudes received by recipient', this.recipient)
+      store.commit('FilterByRecipient', this.recipient)
+      this.rcptEnabled = false
+    },
+    filterResetBySender () {
+      store.commit('FilterReset', 'snd')
+      this.sndEnabled = true
+    },
+    filterResetByRecipient () {
+      store.commit('FilterReset', 'rcpt')
+      this.rcptEnabled = true
     }
   }
 }
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -8,7 +8,11 @@ const store = new Vuex.Store({
     minors: 4,
     validators: 5,
     blocks: [],
-    grat: {}
+    tmpSndBlocks: [],
+    tmpRcptBlocks: [],
+    grat: {},
+    senders: [],
+    recipients: []
   },
   mutations: {
     IncrMinor (state) {
@@ -20,18 +24,79 @@ const store = new Vuex.Store({
     InitBlocks (state, sblocks) {
       console.log('blocks received for initialization', sblocks)
       state.blocks = JSON.parse(sblocks)
+
+      for (let block of state.blocks) {
+        let grat = JSON.parse(atob(block.data))
+        state.senders.push(grat.sender)
+        state.recipients.push(grat.recipient)
+        console.log('sender pushed', grat.sender)
+        console.log('recipient pushed', grat.recipient)
+      }
     },
     UpdateBlocks (state, sblock) {
       console.log('blocks received for update', sblock)
       let block = JSON.parse(sblock)
       state.blocks.push(block)
+
+      let grat = JSON.parse(atob(block.data))
+      state.senders.push(grat.sender)
+      state.recipients.push(grat.recipient)
+      console.log('sender pushed', grat.sender)
+      console.log('recipient pushed', grat.recipient)
     },
     UpdateGrat (state, grat) {
       console.log('grat updated', grat)
       state.grat = grat
+    },
+    FilterBySender (state, sender) {
+      state.tmpSndBlocks = []
+
+      for (let block of state.blocks) {
+        let grat = JSON.parse(atob(block.data))
+        if (grat.sender === sender) {
+          state.tmpSndBlocks.push(block)
+          console.log('block pushed', block)
+        }
+      }
+
+      swapBlocks(state, 'snd')
+    },
+    FilterByRecipient (state, recipient) {
+      state.tmpRcptBlocks = []
+
+      for (let block of state.blocks) {
+        let grat = JSON.parse(atob(block.data))
+        if (grat.recipient === recipient) {
+          state.tmpRcptBlocks.push(block)
+          console.log('block pushed', block)
+        }
+      }
+
+      swapBlocks(state, 'rcpt')
+    },
+
+    FilterReset (state, commEnd) {
+      if (commEnd === 'snd') {
+        state.blocks = state.tmpSndBlocks
+        state.tmpSndBlocks = []
+      } else {
+        state.blocks = state.tmpRcptBlocks
+        state.tmpRcptBlocks = []
+      }
     }
   },
   strict: true
 })
 
+// swaps tmpBlocks with blocks
+function swapBlocks (state, commEnd) {
+  let tmp = state.blocks
+  if (commEnd === 'snd') {
+    state.blocks = state.tmpSndBlocks
+    state.tmpSndBlocks = tmp
+  } else {
+    state.blocks = state.tmpRcptBlocks
+    state.tmpRcptBlocks = tmp
+  }
+}
 export default store
